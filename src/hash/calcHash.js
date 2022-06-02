8c4fc3ffd881bd76d45cd2cd695d0ed36af392e3
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2022-06-03 00:05:50 +0300

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import { readFile } from 'fs/promises';
+import { URL } from 'url';
+
+const { createHash } = await import('crypto');
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filename = getFilePath('./files/fileToCalculateHashFor.txt');
+
+    return readFile(filename, 'utf-8')
+        .then(data => {
+            const hash = createHash('sha256').update(data);
+            const hexValue = hash.digest('hex');
+
+            return hexValue;
+        }
+    );
 };
+
+const getFilePath = (fileName) => {
+    return new URL(fileName, import.meta.url).pathname;
+}
+
+console.log(await calculateHash());
