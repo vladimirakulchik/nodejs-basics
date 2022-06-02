8d2865ca8811c11c82f5a7f92e3ff094c302e39d
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2022-06-02 22:44:05 +0300

    feat: add cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
+import process from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let formattedArgs = [];
+
+    for(let i = 0; i < args.length; i += 2) {
+        formattedArgs.push(`${args[i].substring(2)} is ${args[i + 1]}`);
+    }
+
+    console.log(formattedArgs.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
+import process from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+
+    const env = Object.entries(process.env);
+    const rssEnv = env.map(([key, value]) => {
+        return `${key}=${value}`;
+    }).filter((item) => item.startsWith(prefix));
+
+    const formattedEnv = rssEnv.join('; ');
+
+    if (formattedEnv) {
+        console.log(formattedEnv);
+    }
 };
+
+parseEnv();
