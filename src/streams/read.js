eaedfd4bd2e2217e7108895f2f80d7b9862078c2
Vladimir Akulchik <vladimirakulchyk@gmail.com>
2022-06-04 00:20:15 +0300

    feat: add stream functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import { createReadStream } from 'fs';
+import { resolve, dirname } from 'path';
+import { argv, stdout } from 'process';
+
 export const read = async () => {
-    // Write your code here 
+    const filePath = getFilePath('./files/fileToRead.txt');
+    const input = createReadStream(filePath, 'utf-8');
+
+    input.pipe(stdout);
 };
+
+const getFilePath = (fileName) => {
+    const folder = dirname(argv[1]);
+
+    return resolve(folder, fileName);
+}
+
+read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+
+class ReverseTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+        const data = chunk.toString();
+        const reverseString = data.split('').reverse().join('');
+
+        callback(null, reverseString);
+    }
+}
+
 export const transform = async () => {
-    // Write your code here 
+    const reverseStream = new ReverseTransform();
+
+    stdin.pipe(reverseStream).pipe(stdout);
 };
+
+transform();
